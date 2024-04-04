ab3528b7a217a9d56a13c48b829724d5edb14b4e
Diegodelp <94931438+Diegodelp@users.noreply.github.com>
2024-04-04 00:00:52 -0300

    update

--- carrousel.js
+++ carrousel.js
@@ -1,6 +1,6 @@
-(function(){
+$(function(){
     let current_view = 0;
-    let count_views = ('.carrousel .screens li').length;
+    let count_views = $('.carrousel .screens li').length;
     
     // Methodes
     let switch_view = function(id){
@@ -11,8 +11,8 @@
     let init_pagination = function() {
         for(let i=0; i<count_views; i++) {
             let page = $('<li/>');
-            (page).click(function() {switch_view(i);});
-            ('.carrousel .pagination').append(page);
+            $(page).click(function() {switch_view(i);});
+            $('.carrousel .pagination').append(page);
         }
     };
     let update_view = function(){
@@ -22,12 +22,12 @@
         let view2 = current_view+2;
         if(view0 <= 0) view0 = count_views;
         if(view2 > count_views) view2 = 1;
-        ('.carrousel .screens li').removeClass('left right');
-        ('.carrousel .screens li, .carrousel .pagination li').removeClass('active');
-        ('.carrousel .screens li:nth-child('+view1+')').addClass('active');
-        ('.carrousel .screens li:nth-child('+view0+')').addClass('left');
-        ('.carrousel .screens li:nth-child('+view2+')').addClass('right');
-        ('.carrousel .pagination li:nth-child('+view1+')').addClass('active');
+        $('.carrousel .screens li').removeClass('left right');
+        $('.carrousel .screens li, .carrousel .pagination li').removeClass('active');
+        $('.carrousel .screens li:nth-child('+view1+')').addClass('active');
+        $('.carrousel .screens li:nth-child('+view0+')').addClass('left');
+        $('.carrousel .screens li:nth-child('+view2+')').addClass('right');
+        $('.carrousel .pagination li:nth-child('+view1+')').addClass('active');
     };
     $('.carrousel .screens li').click(function(e){
         let classes = $(e.target).attr('class');
