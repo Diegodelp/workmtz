ae14f27ceb2bae566477ecde485d6599410e9e67
Diegodelp <94931438+Diegodelp@users.noreply.github.com>
2024-04-09 23:57:47 -0300

    update

--- carrousel.js
+++ carrousel.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
-    $("#news-slider").owlCarousel({
-      items: 3,
-      itemsDesktop: [1199, 3],
-      itemsDesktopSmall: [980, 2],
-      itemsMobile: [600, 1],
-      navigation: true,
-      navigationText: ["", ""],
-      pagination: true,
-      autoPlay: true
+    function initCarousel() {
+      if ($("#visible").css("display") == "block") {
+        $(".carousel .carousel-item").each(function () {
+          var i = $(this).next();
+          i.length || (i = $(this).siblings(":first")),
+            i.children(":first-child").clone().appendTo($(this));
+  
+          for (var n = 0; n < 4; n++)
+            (i = i.next()).length || (i = $(this).siblings(":first")),
+              i.children(":first-child").clone().appendTo($(this));
+        });
+      }
+    }
+    $(window).on({
+      resize: initCarousel(),
+      load: initCarousel()
     });
   });
-  
